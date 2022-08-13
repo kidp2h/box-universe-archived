652541592106a9c31efb81da16d3013d0e78226c
kidp2h <kidp2h@gmail.com>
2022-08-13 19:44:12 +0700

    update script

--- scripts/development.sh
+++ scripts/development.sh
@@ -3,10 +3,24 @@ SCRIPT=$(realpath -s "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 PathInstall=$(dirname "$SCRIPTPATH")
 
-echo "Installing client ..."
-git clone https://github.com/kidp2h/box-universe-client "$PathInstall/client" &
->/dev/null
+if [ -d "$PathInstall/client" ]; then
+	echo "Error: Directory client exists. Please remove folder client before execute this script"
+  exit
+else
+	echo "Installing client ..."
+  git clone https://github.com/kidp2h/box-universe-client "$PathInstall/client" &>/dev/null
+  cd "$PathInstall/client"
+  yarn install --ignore-engines &>/dev/null
+  
+fi
+if [ -d "$PathInstall/server" ]; then
+	echo "Error: Directory server exists. Please remove folder server before execute this script"
+  exit
+else
+  echo "Installing server..."
+  git clone https://github.com/kidp2h/box-universe-server "$PathInstall/server" &>/dev/null
+  cd "$PathInstall/server"
+  yarn install --ignore-engines &>/dev/null
+fi
 
-echo "Installing server..."
-git clone https://github.com/kidp2h/box-universe-server "$PathInstall/server" &
->/dev/null
+
---
+++ scripts/production.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+SCRIPT=$(realpath -s "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+PathInstall=$(dirname "$SCRIPTPATH")
+
+if [ -d "$PathInstall/client" ]; then
+	echo "Error: Directory client exists. Please remove folder client before execute this script"
+  exit
+else
+	echo "Installing client, please wait a few seconds"
+  git clone https://github.com/kidp2h/box-universe-client "$PathInstall/client" &>/dev/null
+  cd "$PathInstall/client"
+  yarn install --ignore-engines --production &>/dev/null
+  
+fi
+if [ -d "$PathInstall/server" ]; then
+	echo "Error: Directory server exists. Please remove folder server before execute this script"
+  exit
+else
+  echo "Installing server, please wait a few seconds"
+  git clone https://github.com/kidp2h/box-universe-server "$PathInstall/server" &>/dev/null
+  cd "$PathInstall/server"
+  yarn install --ignore-engines --production &>/dev/null
+fi
+
+echo "Done !!"
