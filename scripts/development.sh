c5b0194aa9948ae120351aad5dc85721375b7191
kidp2h <kidp2h@gmail.com>
2022-08-13 17:49:15 +0700

    update script

--- scripts/development.sh
+++ scripts/development.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-git clone https://github.com/kidp2h/box-universe-client ../client
+parentdir=$(dirname $(dirname "$0"))
+echo $parentdir
+git clone https://github.com/kidp2h/box-universe-client
 git clone https://github.com/kidp2h/box-universe-server ../server
