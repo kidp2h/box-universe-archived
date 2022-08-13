4e23383abd7000dad213ebdf758368ec2d5f8825
kidp2h <kidp2h@gmail.com>
2022-08-13 19:26:33 +0700

    update script

--- scripts/development.sh
+++ scripts/development.sh
@@ -3,5 +3,10 @@ SCRIPT=$(realpath -s "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 PathInstall=$(dirname "$SCRIPTPATH")
 
-git clone https://github.com/kidp2h/box-universe-client "$PathInstall/client"
-git clone https://github.com/kidp2h/box-universe-server "$PathInstall/server"
+echo "Installing client ..."
+git clone https://github.com/kidp2h/box-universe-client "$PathInstall/client" &
+>/dev/null
+
+echo "Installing server..."
+git clone https://github.com/kidp2h/box-universe-server "$PathInstall/server" &
+>/dev/null
